b34da65b161ef0f8352dc5918895b294ee720818
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-14 16:50:00 +0900

    Update 합승_택시_요금.py
    정확성  테스트
    테스트 1 〉	통과 (0.22ms, 10.3MB)
    테스트 2 〉	통과 (0.33ms, 10.3MB)
    테스트 3 〉	통과 (0.20ms, 10.3MB)
    테스트 4 〉	통과 (0.92ms, 10.3MB)
    테스트 5 〉	통과 (1.34ms, 10.3MB)
    테스트 6 〉	통과 (1.79ms, 10.3MB)
    테스트 7 〉	통과 (1.25ms, 10.3MB)
    테스트 8 〉	통과 (2.44ms, 10.4MB)
    테스트 9 〉	통과 (3.48ms, 10.3MB)
    테스트 10 〉	통과 (4.23ms, 10.3MB)
    효율성  테스트
    테스트 1 〉	통과 (461.08ms, 10.3MB)
    테스트 2 〉	통과 (1478.34ms, 10.8MB)
    테스트 3 〉	통과 (3773.58ms, 10.7MB)
    테스트 4 〉	통과 (3450.86ms, 10.8MB)
    테스트 5 〉	통과 (3478.43ms, 10.7MB)
    테스트 6 〉	통과 (3482.96ms, 10.6MB)
    테스트 7 〉	통과 (3643.57ms, 13MB)
    테스트 8 〉	통과 (3683.49ms, 13MB)
    테스트 9 〉	통과 (3818.66ms, 12.9MB)
    테스트 10 〉	통과 (3605.83ms, 13MB)
    테스트 11 〉	통과 (3577.30ms, 12.9MB)
    테스트 12 〉	통과 (3472.65ms, 11.9MB)
    테스트 13 〉	통과 (3502.44ms, 11.9MB)
    테스트 14 〉	통과 (3489.54ms, 11.9MB)
    테스트 15 〉	통과 (3570.76ms, 11.8MB)
    테스트 16 〉	통과 (3464.16ms, 10.7MB)
    테스트 17 〉	통과 (3618.25ms, 10.7MB)
    테스트 18 〉	통과 (3697.07ms, 10.8MB)
    테스트 19 〉	통과 (3518.18ms, 10.8MB)
    테스트 20 〉	통과 (3489.67ms, 10.9MB)
    테스트 21 〉	통과 (3722.67ms, 10.9MB)
    테스트 22 〉	통과 (3507.50ms, 11.9MB)
    테스트 23 〉	통과 (3575.47ms, 11.9MB)
    테스트 24 〉	통과 (3525.95ms, 11.8MB)
    테스트 25 〉	실패 (시간 초과)
    테스트 26 〉	실패 (시간 초과)
    테스트 27 〉	통과 (3410.99ms, 10.9MB)
    테스트 28 〉	통과 (3423.31ms, 10.9MB)
    테스트 29 〉	통과 (429.40ms, 10.4MB)
    테스트 30 〉	통과 (431.34ms, 10.4MB)

--- 합승_택시_요금.py
+++ 합승_택시_요금.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 def solution(n, s, a, b, fares):
-    MAX_INT = 10000000000000
+    MAX_INT = 1000001
     costs = [[MAX_INT for _ in range(n)] for _ in range(n)]
     
     for i in range(n):
@@ -11,7 +11,7 @@ def solution(n, s, a, b, fares):
         costs[x-1][y-1] = fare
         costs[y-1][x-1] = fare
     
-    for start in (s-1,a-1,b-1):
+    for start in (s-1,a-1,b-1,s-1,a-1,b-1):
         bfs = deque([start])
         visited = [False for _ in range(n)]
         while(bfs):
@@ -20,13 +20,15 @@ def solution(n, s, a, b, fares):
             for i in range(n):
                 if not visited[i]:
                     bfs.append(i)
-                if costs[start][i]>costs[start][now]+costs[now][i]:
-                    costs[start][i]=costs[start][now]+costs[now][i]
-                    costs[i][start] = costs[start][i] 
+                temp = costs[start][now]+costs[now][i]
+                if costs[start][i] > temp:
+                    costs[start][i] = temp
+                    costs[i][start] = temp 
     
-    answer = MAX_INT*n+1
+    answer = MAX_INT
     for i in range(n):
-        if answer>costs[s-1][i]+costs[a-1][i]+costs[b-1][i]:
-            answer=costs[s-1][i]+costs[a-1][i]+costs[b-1][i]
+        temp = costs[s-1][i]+costs[a-1][i]+costs[b-1][i]
+        if answer > temp:
+            answer = temp
     
     return answer
