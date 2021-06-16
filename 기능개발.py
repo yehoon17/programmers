81b674859c02b746a0835f794c59baaa3de2b6b2
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-16 09:17:15 +0900

    Create 기능개발.py

---
+++ 기능개발.py
@@ -0,0 +1,18 @@
+def solution(progresses, speeds):
+    answer = []
+    p_stack = progresses[::-1]
+    s_stack = speeds[::-1]
+    days = 0
+    while(p_stack): 
+        speed = s_stack.pop()
+        progress = p_stack.pop() + speed * days
+        count = 1
+        days += -1 * ((progress-100) // speed)
+        while(p_stack and 
+              p_stack[-1]+s_stack[-1]*days>=100):
+            p_stack.pop()
+            s_stack.pop()
+            count+=1
+        
+        answer.append(count)
+    return answer
