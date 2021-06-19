14eec0ae91484b8b89b3042ddc31e06511dd019b
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-20 00:45:36 +0900

    Update 모의고사.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.3MB)
    테스트 2 〉	통과 (0.01ms, 10.3MB)
    테스트 3 〉	통과 (0.01ms, 10.3MB)
    테스트 4 〉	통과 (0.01ms, 10.3MB)
    테스트 5 〉	통과 (0.02ms, 10.1MB)
    테스트 6 〉	통과 (0.02ms, 10.3MB)
    테스트 7 〉	통과 (1.27ms, 10.1MB)
    테스트 8 〉	통과 (0.49ms, 10.2MB)
    테스트 9 〉	통과 (2.05ms, 10.3MB)
    테스트 10 〉	통과 (1.28ms, 10.3MB)
    테스트 11 〉	통과 (2.52ms, 10.3MB)
    테스트 12 〉	통과 (2.30ms, 10.3MB)
    테스트 13 〉	통과 (0.16ms, 10.2MB)
    테스트 14 〉	통과 (2.38ms, 10.3MB)

--- 모의고사.py
+++ 모의고사.py
@@ -1,19 +1,17 @@
 def solution(answers):
     answer = []
     st1 = [1,2,3,4,5]
-    st2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    st3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3] 
+    st2 = [2,1,2,3,2,4,2,5]
+    st3 = [3,3,1,1,2,2,4,4,5,5] 
     score1=0
     score2=0
     score3=0
-    size = len(answers)
-    for i in range(size):
-        a = answers[i]
+    for i,a in enumerate(answers):
         if a == st1[i%5]:
             score1+=1
         if a == st2[i%8]:
             score2+=1
-        if a == st3[i%12]:
+        if a == st3[i%10]:
             score3+=1
     m = max([score1,score2,score3])
     if m == score1:
