770fdf05010956dbb363815aa8937d767677d1c8
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-19 22:40:49 +0900

    Create 네트워크.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.2MB)
    테스트 2 〉	통과 (0.01ms, 10.2MB)
    테스트 3 〉	통과 (0.07ms, 10.3MB)
    테스트 4 〉	통과 (0.07ms, 10.1MB)
    테스트 5 〉	통과 (0.01ms, 10.2MB)
    테스트 6 〉	통과 (0.65ms, 10.2MB)
    테스트 7 〉	통과 (0.02ms, 10MB)
    테스트 8 〉	통과 (0.32ms, 10.2MB)
    테스트 9 〉	통과 (0.14ms, 10.2MB)
    테스트 10 〉	통과 (0.17ms, 10.1MB)
    테스트 11 〉	통과 (1.88ms, 10.3MB)
    테스트 12 〉	통과 (1.30ms, 10.3MB)
    테스트 13 〉	통과 (0.59ms, 10.2MB)

---
+++ 네트워크.py
@@ -0,0 +1,14 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+    for i in range(n):
+        if not visited[i]:
+            bfs = [i]
+            while(bfs):
+                temp = bfs.pop()
+                visited[temp] = True
+                for j in range(n):
+                    if computers[temp][j] == 1 and not visited[j]:
+                        bfs.append(j)
+            answer+=1
+    return answer
