a761165245e553eb53d2d760c2c47b7eb6b9dcc0
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-19 23:40:17 +0900

    Create 단어_변환.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.2MB)
    테스트 2 〉	통과 (0.09ms, 10.2MB)
    테스트 3 〉	통과 (0.33ms, 10.2MB)
    테스트 4 〉	통과 (0.01ms, 10.3MB)
    테스트 5 〉	통과 (0.00ms, 10.3MB)

---
+++ 단어_변환.py
@@ -0,0 +1,32 @@
+def isTran(word1, word2):
+    count = 0
+    for c1, c2 in zip(word1, word2):
+        if not c1 == c2:
+            count +=1
+            if count > 1:
+                return False
+    if count == 0:
+        return False
+    return True
+
+def dfs(begin, target, words, visited):
+    if begin == target:
+        return 0
+    visited[begin] = True
+    temp = len(words)+1
+    for word in words:
+        if not visited[word]:
+            if isTran(begin, word):
+                temp = min(temp,
+                           dfs(word, target, words, visited)+1)
+    visited[begin] = False
+    return temp
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    visited = {word:False for word in words}
+    answer = dfs(begin, target, words, visited)
+    if answer>len(words):
+        return 0
+    return answer
