2b11b58a939cd3e490a680dad3213ae51066eaeb
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-20 20:10:19 +0900

    Create 타일링3xn.py
    정확성  테스트
    테스트 1 〉	통과 (47.91ms, 10.4MB)
    테스트 2 〉	통과 (78.59ms, 10.4MB)
    테스트 3 〉	통과 (170.88ms, 10.8MB)
    테스트 4 〉	통과 (194.09ms, 10.7MB)
    테스트 5 〉	통과 (0.32ms, 10.3MB)
    테스트 6 〉	통과 (27.85ms, 10.3MB)
    테스트 7 〉	통과 (67.82ms, 10.4MB)
    테스트 8 〉	통과 (143.62ms, 10.5MB)
    테스트 9 〉	통과 (89.49ms, 10.4MB)
    테스트 10 〉	통과 (21.80ms, 10.2MB)
    테스트 11 〉	통과 (54.98ms, 10.1MB)
    테스트 12 〉	통과 (12.40ms, 10.3MB)
    테스트 13 〉	통과 (28.17ms, 10.3MB)
    테스트 14 〉	통과 (27.61ms, 10.3MB)
    효율성  테스트
    테스트 1 〉	통과 (170.83ms, 10.7MB)
    테스트 2 〉	통과 (191.19ms, 10.7MB)
    테스트 3 〉	통과 (183.34ms, 10.4MB)
    테스트 4 〉	통과 (179.96ms, 10.6MB)
    테스트 5 〉	통과 (233.74ms, 10.8MB)
    테스트 6 〉	통과 (158.39ms, 10.7MB)

---
+++ 타일링3xn.py
@@ -0,0 +1,9 @@
+def solution(n):
+    if n%2 == 1:
+        return 0
+    n=n//2-1
+    dp = [3]
+    for i in range(n):
+        temp = 3*dp[-1] + 2 + sum(dp[:-1])*2
+        dp.append(temp)
+    return dp[n]%1000000007
