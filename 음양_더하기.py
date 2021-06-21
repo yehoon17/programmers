710bd2d512207f7ec020d0cd1ba2c4f0d4dba8d2
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-21 09:20:29 +0900

    Create 음양_더하기.py
    정확성  테스트
    테스트 1 〉	통과 (0.08ms, 10.1MB)
    테스트 2 〉	통과 (0.09ms, 10.1MB)
    테스트 3 〉	통과 (0.09ms, 10.2MB)
    테스트 4 〉	통과 (0.08ms, 10.3MB)
    테스트 5 〉	통과 (0.10ms, 10.3MB)
    테스트 6 〉	통과 (0.08ms, 10.3MB)
    테스트 7 〉	통과 (0.08ms, 10.4MB)
    테스트 8 〉	통과 (0.10ms, 10.2MB)
    테스트 9 〉	통과 (0.09ms, 10.1MB)

---
+++ 음양_더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for n, sign in zip(absolutes, signs):
+        if sign:
+            answer+=n
+        else:
+            answer-=n
+    return answer
