b885db1e9f66758fac112ea1fdcc04e261d01aa3
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-22 17:15:28 +0900

    Create 같은_숫자는_싫어.py
    정확성  테스트
    테스트 1 〉	통과 (0.00ms, 10.2MB)
    테스트 2 〉	통과 (0.02ms, 10.2MB)
    테스트 3 〉	통과 (0.01ms, 10.2MB)
    테스트 4 〉	통과 (0.01ms, 10.2MB)
    테스트 5 〉	통과 (0.01ms, 10.1MB)
    테스트 6 〉	통과 (0.01ms, 10.2MB)
    테스트 7 〉	통과 (0.01ms, 10.2MB)
    테스트 8 〉	통과 (0.01ms, 10.2MB)
    테스트 9 〉	통과 (0.02ms, 10.2MB)
    테스트 10 〉	통과 (0.01ms, 10.2MB)
    테스트 11 〉	통과 (0.01ms, 10.2MB)
    테스트 12 〉	통과 (0.01ms, 10.1MB)
    테스트 13 〉	통과 (0.01ms, 10.3MB)
    테스트 14 〉	통과 (0.01ms, 10.2MB)
    테스트 15 〉	통과 (0.01ms, 10.1MB)
    테스트 16 〉	통과 (0.02ms, 10.1MB)
    테스트 17 〉	통과 (0.00ms, 10.2MB)
    효율성  테스트
    테스트 1 〉	통과 (133.23ms, 27.8MB)
    테스트 2 〉	통과 (60.78ms, 28MB)
    테스트 3 〉	통과 (60.20ms, 27.8MB)
    테스트 4 〉	통과 (62.45ms, 28MB)

---
+++ 같은_숫자는_싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in arr:
+        if answer:
+            if not answer[-1] == i:
+                answer.append(i)
+        else:
+            answer.append(i)
+    return answer
