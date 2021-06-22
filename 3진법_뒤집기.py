d9bac44bf86c838fa913bbc304399d084bf0c2df
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-22 17:10:05 +0900

    Create 3진법_뒤집기.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.3MB)
    테스트 2 〉	통과 (0.01ms, 10.1MB)
    테스트 3 〉	통과 (0.01ms, 10.2MB)
    테스트 4 〉	통과 (0.01ms, 10.2MB)
    테스트 5 〉	통과 (0.01ms, 10.2MB)
    테스트 6 〉	통과 (0.01ms, 10.2MB)
    테스트 7 〉	통과 (0.01ms, 10.1MB)
    테스트 8 〉	통과 (0.01ms, 10.1MB)
    테스트 9 〉	통과 (0.01ms, 10.2MB)
    테스트 10 〉	통과 (0.01ms, 10MB)

---
+++ 3진법_뒤집기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    l=[]
+    while(n>0):
+        l.append(n%3)
+        n = n//3
+    answer = 0
+    x = 1
+    for i in range(len(l)-1,-1,-1):
+        answer+=l[i]*x
+        x*=3
+    return answer
