45eb6a5b70ea6cb6b126da5c9cc806976e0e06ef
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-23 17:40:51 +0900

    Create 이진_변환_반복하기.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.2MB)
    테스트 2 〉	실패 (0.36ms, 10.4MB)
    테스트 3 〉	통과 (0.00ms, 10.2MB)
    테스트 4 〉	통과 (0.01ms, 10.1MB)
    테스트 5 〉	통과 (0.01ms, 10.2MB)
    테스트 6 〉	통과 (0.01ms, 10.2MB)
    테스트 7 〉	통과 (0.02ms, 10.2MB)
    테스트 8 〉	통과 (0.01ms, 10.2MB)
    테스트 9 〉	통과 (0.33ms, 10.3MB)
    테스트 10 〉	통과 (1.13ms, 10.4MB)
    테스트 11 〉	통과 (0.51ms, 10.3MB)

---
+++ 이진_변환_반복하기.py
@@ -0,0 +1,19 @@
+def solution(s):
+    zeros=s.count('0')
+    n=s.count('1')
+    repeat=1
+    while(True):
+        temp=0
+        while(True):
+            if n%2==0:
+                zeros+=1
+            else:
+                temp+=1
+            n//=2
+            if n==0:
+                break
+        n=temp
+        repeat+=1
+        if temp==1:
+            break
+    return[repeat,zeros]
