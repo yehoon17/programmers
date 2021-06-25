f65dd04a93fffc44d8b6455bf9f65709f6847c91
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-06-25 12:47:10 +0900

    Update 소수_찾기.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.1MB)
    테스트 2 〉	통과 (0.88ms, 10.2MB)
    테스트 3 〉	통과 (0.12ms, 10.3MB)
    테스트 4 〉	통과 (0.25ms, 10.3MB)
    테스트 5 〉	통과 (0.16ms, 10.2MB)
    테스트 6 〉	통과 (1.86ms, 10.3MB)
    테스트 7 〉	통과 (0.80ms, 10.3MB)
    테스트 8 〉	통과 (1.47ms, 10.3MB)
    테스트 9 〉	통과 (2.16ms, 10.2MB)
    테스트 10 〉	통과 (55.33ms, 12.1MB)
    테스트 11 〉	통과 (183.76ms, 16.9MB)
    테스트 12 〉	통과 (75.73ms, 12.4MB)
    효율성  테스트
    테스트 1 〉	통과 (195.66ms, 17.4MB)
    테스트 2 〉	통과 (190.20ms, 17.1MB)
    테스트 3 〉	통과 (194.84ms, 17.1MB)
    테스트 4 〉	통과 (190.73ms, 17MB)
    채점 결과
    정확성: 75.0
    효율성: 25.0
    합계: 100.0 / 100.0

--- 소수_찾기.py
+++ 소수_찾기.py
@@ -3,7 +3,10 @@ def solution(n):
     temp[0] = 0
     for i in range(2,int(n**0.5)+1):
         if temp[i-1] == 1:
-            for j in range(i+1,n+1):
-                if j%i==0:
-                    temp[j-1]=0
+            p=i
+            while(True):
+                p+=i
+                if p>n:
+                    break
+                temp[p-1]=0
     return sum(temp)
