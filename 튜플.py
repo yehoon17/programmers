e8e96246f9a14b12d9735d45324d69afc6973161
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-05 15:17:35 +0900

    Create 튜플.py
    정확성  테스트
    테스트 1 〉	통과 (0.04ms, 10.4MB)
    테스트 2 〉	통과 (0.02ms, 10.3MB)
    테스트 3 〉	통과 (0.03ms, 10.4MB)
    테스트 4 〉	통과 (0.04ms, 10.4MB)
    테스트 5 〉	통과 (0.24ms, 10.3MB)
    테스트 6 〉	통과 (0.66ms, 10.4MB)
    테스트 7 〉	통과 (10.53ms, 10.5MB)
    테스트 8 〉	통과 (53.32ms, 11.1MB)
    테스트 9 〉	통과 (18.81ms, 10.7MB)
    테스트 10 〉	통과 (56.01ms, 11.2MB)
    테스트 11 〉	통과 (84.76ms, 11.5MB)
    테스트 12 〉	통과 (143.14ms, 12MB)
    테스트 13 〉	통과 (130.93ms, 12.1MB)
    테스트 14 〉	통과 (148.74ms, 12.1MB)
    테스트 15 〉	통과 (0.03ms, 10.4MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

---
+++ 튜플.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = []
+    temp = s[2:-2].split('},{')
+    temp.sort(key=lambda x:x.count(','))
+    for x in temp:
+        l = x.split(',')
+        for i in l:
+            if int(i) not in answer:
+                answer.append(int(i))
+                break
+    return answer
