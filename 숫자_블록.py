d0217b8da7149fcfb162c536b76d26c4a05ba9d4
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-07 12:44:57 +0900

    Create 숫자_블록.py
    정확성  테스트
    테스트 1 〉	통과 (11.18ms, 10.4MB)
    테스트 2 〉	통과 (44.53ms, 10.8MB)
    테스트 3 〉	통과 (35.79ms, 10.7MB)
    테스트 4 〉	통과 (35.11ms, 10.9MB)
    테스트 5 〉	통과 (37.76ms, 10.7MB)
    테스트 6 〉	통과 (7.40ms, 10.4MB)
    테스트 7 〉	통과 (33.50ms, 10.8MB)
    테스트 8 〉	통과 (13.43ms, 10.3MB)
    테스트 9 〉	통과 (34.12ms, 10.7MB)
    테스트 10 〉	통과 (40.08ms, 10.7MB)
    테스트 11 〉	통과 (13.19ms, 10.3MB)
    테스트 12 〉	통과 (22.07ms, 10.5MB)
    테스트 13 〉	통과 (22.92ms, 10.6MB)
    테스트 14 〉	통과 (47.94ms, 10.9MB)
    효율성  테스트
    테스트 1 〉	실패 (시간 초과)
    테스트 2 〉	실패 (시간 초과)
    테스트 3 〉	실패 (시간 초과)
    테스트 4 〉	실패 (시간 초과)
    테스트 5 〉	실패 (시간 초과)
    테스트 6 〉	실패 (시간 초과)
    채점 결과
    정확성: 70.0
    효율성: 0.0
    합계: 70.0 / 100.0

---
+++ 숫자_블록.py
@@ -0,0 +1,29 @@
+def solution(begin, end):
+    answer = []
+    isPrime = [True for _ in range(end+1)]
+    primes = []
+    i=2
+    while(True):
+        if i>len(isPrime)-1:
+            break
+        if isPrime[i]:
+            primes.append(i)
+            temp = i
+            while(True):
+                temp+=i
+                if temp>len(isPrime)-1:
+                    break
+                isPrime[temp]=False
+        i+=1
+    for i in range(begin,end+1):
+        if i==1:
+            answer.append(0)
+            continue
+        if i in primes:
+            answer.append(1)
+        else:
+            for p in primes:
+                if i%p==0:
+                    answer.append(i//p)
+                    break
+    return answer
