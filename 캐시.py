79f2c2aac2ba05a345ba398d507fbbe6dcca0a35
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-14 09:27:39 +0900

    Update 캐시.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.2MB)
    테스트 2 〉	통과 (0.01ms, 10.2MB)
    테스트 3 〉	통과 (0.01ms, 10.2MB)
    테스트 4 〉	통과 (0.01ms, 10.2MB)
    테스트 5 〉	통과 (0.01ms, 10.2MB)
    테스트 6 〉	통과 (0.01ms, 10.2MB)
    테스트 7 〉	통과 (0.01ms, 10.2MB)
    테스트 8 〉	통과 (0.01ms, 10.3MB)
    테스트 9 〉	통과 (0.01ms, 10.2MB)
    테스트 10 〉	통과 (0.04ms, 10.2MB)
    테스트 11 〉	실패 (29.90ms, 17.6MB)
    테스트 12 〉	통과 (0.05ms, 10.2MB)
    테스트 13 〉	통과 (0.09ms, 10.2MB)
    테스트 14 〉	통과 (0.13ms, 10.2MB)
    테스트 15 〉	통과 (0.20ms, 10.2MB)
    테스트 16 〉	실패 (0.26ms, 10.3MB)
    테스트 17 〉	통과 (0.56ms, 10.2MB)
    테스트 18 〉	통과 (0.58ms, 10.2MB)
    테스트 19 〉	실패 (0.54ms, 10.3MB)
    테스트 20 〉	실패 (0.56ms, 10.2MB)
    채점 결과
    정확성: 80.0
    합계: 80.0 / 100.0

--- 캐시.py
+++ 캐시.py
@@ -1,13 +1,16 @@
 def solution(cacheSize, cities):
     answer = 0
     cache = {}
+    temp = 0
     for i, city in enumerate(cities):
         city = city.lower()
         if city in cache:
-            if cache[city] > i-cacheSize-1:
+            if cache[city] > i-cacheSize-1-temp:
                 answer+=1
+                temp+=1
             else:
                 answer+=5
+                temp=0
         else:
             answer+=5
         cache[city] = i
