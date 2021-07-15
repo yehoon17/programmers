a0b75d8b00d74ed3024cc62742b0a6fe61d41967
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-15 09:42:27 +0900

    Create 쿠키_구입.py
    IndexError: list index out of range

---
+++ 쿠키_구입.py
@@ -0,0 +1,22 @@
+def solution(cookie):
+    size = len(cookie)
+    temp = [[0 for _ in range(size)] for _ in range(size)]
+    
+    for i in range(size):
+        for j in range(size):
+            if j == 0:
+                temp[i][i+j] = cookie[i]
+            elif i+j<size:
+                temp[i][i+j] = temp[i][i+j-1]+cookie[i+j]
+            else:
+                k = i-1-(size-i-j)
+                temp[i][k] = temp[i][k+1]+cookie[k]
+                
+    answer = 0
+    for i in range(size):
+        for j in range(i,size):
+            for k in range(j+1,size):
+                if temp[i][j] == temp[j+1][k]:
+                    answer+=1
+                    break
+    return answer
