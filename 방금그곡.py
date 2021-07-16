57111885db805205758d1bafaad827b85ebb50a5
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-16 10:08:25 +0900

    Update 방금그곡.py
    정확성  테스트
    테스트 1 〉	통과 (0.03ms, 10.3MB)
    테스트 2 〉	실패 (0.04ms, 10.5MB)
    테스트 3 〉	통과 (0.03ms, 10.4MB)
    테스트 4 〉	통과 (0.03ms, 10.4MB)
    테스트 5 〉	통과 (0.04ms, 10.4MB)
    테스트 6 〉	실패 (0.04ms, 10.4MB)
    테스트 7 〉	통과 (0.04ms, 10.4MB)
    테스트 8 〉	통과 (0.05ms, 10.4MB)
    테스트 9 〉	통과 (0.05ms, 10.3MB)
    테스트 10 〉	통과 (0.36ms, 10.4MB)
    테스트 11 〉	통과 (0.15ms, 10.4MB)
    테스트 12 〉	통과 (0.06ms, 10.4MB)
    테스트 13 〉	통과 (0.04ms, 10.4MB)
    테스트 14 〉	통과 (0.08ms, 10.4MB)
    테스트 15 〉	통과 (0.10ms, 10.4MB)
    테스트 16 〉	통과 (0.07ms, 10.4MB)
    테스트 17 〉	통과 (0.11ms, 10.4MB)
    테스트 18 〉	통과 (0.05ms, 10.5MB)
    테스트 19 〉	통과 (0.86ms, 10.4MB)
    테스트 20 〉	실패 (0.06ms, 10.4MB)
    테스트 21 〉	실패 (0.03ms, 10.4MB)
    테스트 22 〉	실패 (0.14ms, 10.4MB)
    테스트 23 〉	실패 (0.15ms, 10.4MB)
    테스트 24 〉	실패 (0.16ms, 10.5MB)
    테스트 25 〉	통과 (0.04ms, 10.4MB)
    테스트 26 〉	통과 (0.04ms, 10.5MB)
    테스트 27 〉	통과 (0.04ms, 10.4MB)
    테스트 28 〉	통과 (0.05ms, 10.4MB)
    테스트 29 〉	실패 (7.75ms, 10.4MB)
    테스트 30 〉	실패 (1.18ms, 10.4MB)
    채점 결과
    정확성: 70.0
    합계: 70.0 / 100.0

--- 방금그곡.py
+++ 방금그곡.py
@@ -1,9 +1,26 @@
+def modify(x):
+    i=0
+    x_=''
+    while(i<len(x)-1):
+        if x[i+1]=='#':
+            x_+=x[i].lower()
+            i+=2
+        else:
+            x_+=x[i]
+            i+=1
+            if i==len(x)-1:
+                x_+=x[i]
+    return x_
+    
+
 def solution(m, musicinfos):
-    answer = ''
+    answer = ''  
+    m_=modify(m)
     for musicinfo in musicinfos:
         start, end, title, chord = musicinfo.split(',')
+        chord_=modify(chord)
         playtime = (int(end[:2])-int(start[:2]))*60+int(end[-2:])-int(start[-2:])
-        music = chord*(playtime//len(chord))+chord[:playtime%len(chord)]
-        if m in music:
+        music = chord_*(playtime//len(chord_))+chord_[:playtime%len(chord_)]
+        if m_ in music:
             return title
     return answer
