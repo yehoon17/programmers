4d849bc1fb0eecd184561d80daef51e9816c3934
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-17 16:53:42 +0900

    Update 불량_사용자.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.3MB)
    테스트 2 〉	통과 (0.03ms, 10.2MB)
    테스트 3 〉	통과 (0.04ms, 10.3MB)
    테스트 4 〉	통과 (0.03ms, 10.2MB)
    테스트 5 〉	통과 (1.75ms, 10.2MB)
    테스트 6 〉	통과 (0.39ms, 10.3MB)
    테스트 7 〉	통과 (0.03ms, 10.4MB)
    테스트 8 〉	통과 (0.04ms, 10.1MB)
    테스트 9 〉	통과 (0.06ms, 10.2MB)
    테스트 10 〉	통과 (0.03ms, 10.2MB)
    테스트 11 〉	통과 (0.05ms, 10.2MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- 불량_사용자.py
+++ 불량_사용자.py
@@ -1,8 +1,7 @@
 def solution(user_id, banned_id):
-    answer = 0
-    dic={}
+    cases = []
     for x in banned_id:
-        dic[x]=[]
+        candidate=[]
         for y in user_id:
             if len(x) == len(y):
                 isMatch = True
@@ -13,8 +12,16 @@ def solution(user_id, banned_id):
                         isMatch = False
                         break
                 if isMatch:
-                    dic[x].append(y)
-                    
-    
-                        
-    return answer
+                    candidate.append(y)
+        if not cases:
+            cases = [set([id_]) for id_ in candidate]
+        else:
+            temp = []
+            for case in cases:
+                for id_ in candidate:
+                    if id_ not in case:
+                        new_case = case|set([id_])
+                        if new_case not in temp:
+                            temp.append(new_case)
+            cases = temp
+    return len(cases)
