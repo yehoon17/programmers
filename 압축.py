2f3b7fe371880ab5ccb5fe70c7bcfb9d37fc9031
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-17 16:08:59 +0900

    Update 압축.py
    정확성  테스트
    테스트 1 〉	통과 (0.02ms, 10.1MB)
    테스트 2 〉	통과 (0.04ms, 10.3MB)
    테스트 3 〉	통과 (0.03ms, 10.2MB)
    테스트 4 〉	통과 (0.40ms, 10.2MB)
    테스트 5 〉	통과 (0.04ms, 10.1MB)
    테스트 6 〉	통과 (0.74ms, 10.2MB)
    테스트 7 〉	통과 (0.49ms, 10.1MB)
    테스트 8 〉	통과 (0.53ms, 10.2MB)
    테스트 9 〉	통과 (0.01ms, 10.1MB)
    테스트 10 〉	통과 (0.52ms, 10.2MB)
    테스트 11 〉	통과 (0.37ms, 10.2MB)
    테스트 12 〉	통과 (0.60ms, 10.1MB)
    테스트 13 〉	통과 (0.52ms, 10.1MB)
    테스트 14 〉	통과 (0.84ms, 10.2MB)
    테스트 15 〉	통과 (0.87ms, 10.2MB)
    테스트 16 〉	통과 (0.68ms, 10.3MB)
    테스트 17 〉	통과 (0.52ms, 10.1MB)
    테스트 18 〉	통과 (0.20ms, 10.2MB)
    테스트 19 〉	통과 (0.25ms, 10.2MB)
    테스트 20 〉	통과 (0.51ms, 10.1MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- 압축.py
+++ 압축.py
@@ -2,17 +2,16 @@ def solution(msg):
     answer = []
     dic = {chr(ord('A')+i):i+1 for i in range(26)}
     val = 27
-    i, n = 0, 1
+    i = 0
     while(i<len(msg)):
-        key = msg[i:i+n]
-        if key in dic:
-            answer.append(dic[key])
-            dic.pop(key)
-            if i+n<len(msg):
-                dic[msg[i:i+n+1]] = val
-                val+=1
-            i+=n
-            n = 1
-        else:
+        n=0
+        while(i+n<len(msg)):
+            key = msg[i:i+n+1]
+            if key not in dic:
+                break
             n+=1
+        answer.append(dic[msg[i:i+n]])
+        dic[key] = val
+        val+=1
+        i+=n
     return answer
