671282e484315b426e9f211e46f0ae678b3ccedb
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-18 17:15:38 +0900

    Update 줄_서는_방법.py
    정확성  테스트
    테스트 1 〉	통과 (0.06ms, 10.1MB)
    테스트 2 〉	통과 (14.23ms, 10.2MB)
    테스트 3 〉	통과 (6.14ms, 10.2MB)
    테스트 4 〉	통과 (0.02ms, 10.2MB)
    테스트 5 〉	통과 (0.01ms, 10.1MB)
    테스트 6 〉	통과 (0.01ms, 10.2MB)
    테스트 7 〉	통과 (0.55ms, 10.2MB)
    테스트 8 〉	통과 (0.04ms, 10.1MB)
    테스트 9 〉	통과 (0.01ms, 10.2MB)
    테스트 10 〉	통과 (156.79ms, 10.2MB)
    테스트 11 〉	통과 (233.74ms, 10.1MB)
    테스트 12 〉	통과 (343.92ms, 10.2MB)
    테스트 13 〉	통과 (633.14ms, 10.3MB)
    테스트 14 〉	통과 (0.08ms, 10.2MB)
    효율성  테스트
    테스트 1 〉	실패 (시간 초과)
    테스트 2 〉	실패 (시간 초과)
    테스트 3 〉	실패 (시간 초과)
    테스트 4 〉	실패 (시간 초과)
    테스트 5 〉	실패 (시간 초과)
    채점 결과
    정확성: 73.7
    효율성: 0.0
    합계: 73.7 / 100.0

--- 줄_서는_방법.py
+++ 줄_서는_방법.py
@@ -1,24 +1,9 @@
+from itertools import permutations
+
 def solution(n, k):
-    answer=[]
-    
-    fact = [1]
-    for i in range(1,n):
-        fact.append(fact[-1]*i)
-        
-    nums = [False for _ in range(n)]
-    
-    for m in range(n):
-        if fact[-1-m]==1:
-            i=1
-        else:
-            i = k//fact[-1-m]+1
-        for j,num in enumerate(nums):
-            if not num:
-                i-=1
-            if i==0:
-                answer.append(j+1)
-                nums[j]=True
-                break
-        k = k%fact[-1-m]
-        
-    return answer
+    l=[i for i in range(1,1+n)]
+    a=0
+    for p in permutations(l):
+        a+=1
+        if a==k:
+            return list(p)
