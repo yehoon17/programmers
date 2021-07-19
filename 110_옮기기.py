c1af9b5725d7120e0cf584d98a5b830ebe77e237
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-19 12:11:03 +0900

    Update 110_옮기기.py
    정확성  테스트
    테스트 1 〉	통과 (34.70ms, 15.6MB)
    테스트 2 〉	통과 (35.34ms, 15.6MB)
    테스트 3 〉	통과 (29.76ms, 13.9MB)
    테스트 4 〉	통과 (43.40ms, 16.1MB)
    테스트 5 〉	통과 (23.79ms, 15.5MB)
    테스트 6 〉	통과 (25.28ms, 15.5MB)
    테스트 7 〉	통과 (22.25ms, 13.7MB)
    테스트 8 〉	통과 (15.26ms, 12.9MB)
    테스트 9 〉	통과 (61.72ms, 30.4MB)
    테스트 10 〉	통과 (58.42ms, 32.1MB)
    테스트 11 〉	통과 (53.49ms, 30.7MB)
    테스트 12 〉	통과 (56.71ms, 30.6MB)
    테스트 13 〉	통과 (52.19ms, 30.4MB)
    테스트 14 〉	통과 (66.96ms, 30.1MB)
    테스트 15 〉	통과 (53.84ms, 30.2MB)
    테스트 16 〉	통과 (54.47ms, 30.2MB)
    테스트 17 〉	통과 (46.23ms, 29.2MB)
    테스트 18 〉	실패 (시간 초과)
    테스트 19 〉	실패 (시간 초과)
    테스트 20 〉	실패 (시간 초과)
    테스트 21 〉	실패 (시간 초과)
    채점 결과
    정확성: 81.0
    합계: 81.0 / 100.0

--- 110_옮기기.py
+++ 110_옮기기.py
@@ -1,12 +1,12 @@
-def remove_all(pattern,string):
-    while(pattern in string):
-        string = string.replace(pattern,'')
-    return string
-    
 def solution(string):
     answer = []
     for s in string:
-        temp = '0'+ remove_all('110', s)
+        temp = s
+        while('110' in temp):
+            temp = temp.replace('111100','')
+            temp = temp.replace('110','')
+            temp = temp.replace('110','')
+        temp = '0' + temp
         count = (len(s) - len(temp) + 1)//3
         i = temp.rfind('0')
         s=temp[:i+1]+ '110'*count +temp[i+1:]
