165979327f6cf26a4f07379c6cbd2ce2d66aabc2
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-21 16:28:21 +0900

    Update 징검다리_건너기.py
    정확성  테스트
    테스트 1 〉	통과 (0.00ms, 10.2MB)
    테스트 2 〉	통과 (0.00ms, 10.2MB)
    테스트 3 〉	실패 (0.02ms, 10.2MB)
    테스트 4 〉	통과 (0.08ms, 10.1MB)
    테스트 5 〉	통과 (0.17ms, 10.2MB)
    테스트 6 〉	통과 (1.67ms, 10.2MB)
    테스트 7 〉	통과 (5.81ms, 10.2MB)
    테스트 8 〉	통과 (13.04ms, 10.2MB)
    테스트 9 〉	통과 (24.95ms, 10.2MB)
    테스트 10 〉	통과 (0.05ms, 10.2MB)
    테스트 11 〉	통과 (0.02ms, 10.2MB)
    테스트 12 〉	통과 (0.08ms, 10.1MB)
    테스트 13 〉	통과 (0.17ms, 10.3MB)
    테스트 14 〉	통과 (1.57ms, 10.2MB)
    테스트 15 〉	통과 (5.63ms, 10.2MB)
    테스트 16 〉	통과 (12.48ms, 10.3MB)
    테스트 17 〉	통과 (25.27ms, 10.2MB)
    테스트 18 〉	통과 (0.02ms, 10.2MB)
    테스트 19 〉	통과 (0.08ms, 10.2MB)
    테스트 20 〉	통과 (0.16ms, 10.1MB)
    테스트 21 〉	통과 (1.65ms, 10.2MB)
    테스트 22 〉	통과 (5.70ms, 10.3MB)
    테스트 23 〉	통과 (12.13ms, 10.1MB)
    테스트 24 〉	통과 (28.23ms, 10.2MB)
    테스트 25 〉	통과 (0.03ms, 10.2MB)
    효율성  테스트
    테스트 1 〉	실패 (시간 초과)
    테스트 2 〉	실패 (시간 초과)
    테스트 3 〉	실패 (시간 초과)
    테스트 4 〉	실패 (시간 초과)
    테스트 5 〉	실패 (시간 초과)
    테스트 6 〉	실패 (시간 초과)
    테스트 7 〉	실패 (시간 초과)
    테스트 8 〉	실패 (시간 초과)
    테스트 9 〉	실패 (시간 초과)
    테스트 10 〉	실패 (시간 초과)
    테스트 11 〉	실패 (시간 초과)
    테스트 12 〉	실패 (시간 초과)
    테스트 13 〉	실패 (시간 초과)
    테스트 14 〉	실패 (시간 초과)
    채점 결과
    정확성: 61.5
    효율성: 0.0
    합계: 61.5 / 100.0

--- 징검다리_건너기.py
+++ 징검다리_건너기.py
@@ -1,6 +1,9 @@
 def solution(stones, k):
     answer = 200001
-    for i in range(len(stones)-k):
-        if answer>max(stones[i:i+k]):
-            answer=max(stones[i:i+k])
-    return answer
+    stones_max = stones
+    for i in range(1,k):
+        temp = []
+        for j in range(len(stones)-i-1):
+            temp.append(max(stones_max[j:j+2]))
+        stones_max = temp
+    return min(stones_max)
