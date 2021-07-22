81abab3dcc1397afe4e300d3ef058e32b1b5df44
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-22 11:30:26 +0900

    Update 여행경로.py
    정확성  테스트
    테스트 1 〉	통과 (261.55ms, 10.4MB)
    테스트 2 〉	통과 (0.01ms, 10.2MB)
    테스트 3 〉	통과 (0.01ms, 10.3MB)
    테스트 4 〉	통과 (0.01ms, 10.2MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- 여행경로.py
+++ 여행경로.py
@@ -1,5 +1,6 @@
 def dfs(begin, tickets, visited, count):
     min = ['ZZZ']
+    flag = False
     for i, [dep, arr] in enumerate(tickets):
         if not visited[i]:
             if begin == dep:
@@ -7,13 +8,19 @@ def dfs(begin, tickets, visited, count):
                     return [arr,begin]
                 visited[i] = True
                 temp = dfs(arr, tickets, visited, count-1)
-                if min[-1] > temp[-1]:
-                    min = temp
+                if temp:
+                    if min[-1] > temp[-1]:
+                        flag = True
+                        min = temp
                 visited[i] = False
-    min.append(begin)
-    return min
-
+    if flag:    
+        min.append(begin)
+        return min
+    return []
+    
 def solution(tickets):
     size = len(tickets)
     visited = [False for _ in range(size)]
     return dfs('ICN',tickets, visited, size)[::-1]
+
+solution([["ICN", "B"], ["B", "ICN"], ["ICN", "A"], ["A", "D"], ["D", "A"]])
