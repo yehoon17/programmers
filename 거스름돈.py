bbcae67019fb1ced8de1e78bd2211fc93d562dea
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-23 09:31:17 +0900

    Create 거스름돈.py
    정확성  테스트
    테스트 1 〉	통과 (1.18ms, 10.2MB)
    테스트 2 〉	통과 (1.11ms, 10.2MB)
    테스트 3 〉	통과 (1.13ms, 10.2MB)
    테스트 4 〉	통과 (0.75ms, 10.2MB)
    테스트 5 〉	통과 (0.07ms, 10.3MB)
    테스트 6 〉	통과 (0.05ms, 10.2MB)
    테스트 7 〉	통과 (2.17ms, 10.1MB)
    테스트 8 〉	통과 (5.66ms, 10.3MB)
    테스트 9 〉	통과 (5.67ms, 10.3MB)
    테스트 10 〉	통과 (0.23ms, 10.2MB)
    테스트 11 〉	통과 (1.16ms, 10.3MB)
    테스트 12 〉	통과 (1.90ms, 10.2MB)
    테스트 13 〉	통과 (0.01ms, 10.2MB)
    테스트 14 〉	통과 (2.70ms, 10.2MB)
    효율성  테스트
    테스트 1 〉	실패 (시간 초과)
    테스트 2 〉	실패 (시간 초과)
    테스트 3 〉	실패 (시간 초과)
    테스트 4 〉	실패 (시간 초과)
    테스트 5 〉	실패 (시간 초과)
    테스트 6 〉	실패 (시간 초과)
    채점 결과
    정확성: 70.0
    효율성: 0.0
    합계: 70.0 / 100.0

---
+++ 거스름돈.py
@@ -0,0 +1,14 @@
+def solution(n, money):
+    answer = 0
+    dp = [n]
+    for x in money:
+        temp = []
+        for y in dp:
+            z = y
+            while(z>0):
+                temp.append(z)
+                z-=x
+                if z == 0:
+                    answer+=1
+        dp = temp
+    return answer%1000000007
