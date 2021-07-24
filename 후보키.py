a3a2fc8deb38797839050f5873f99178b3548768
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-07-24 22:15:21 +0900

    Create 후보키.py
    정확성  테스트
    테스트 1 〉	통과 (0.04ms, 10.2MB)
    테스트 2 〉	통과 (0.05ms, 10.3MB)
    테스트 3 〉	통과 (0.05ms, 10.2MB)
    테스트 4 〉	통과 (0.08ms, 10.2MB)
    테스트 5 〉	통과 (0.08ms, 10.2MB)
    테스트 6 〉	통과 (0.02ms, 10.2MB)
    테스트 7 〉	통과 (0.02ms, 10.4MB)
    테스트 8 〉	통과 (0.02ms, 10.2MB)
    테스트 9 〉	통과 (0.10ms, 10.4MB)
    테스트 10 〉	통과 (0.06ms, 10.2MB)
    테스트 11 〉	통과 (0.17ms, 10.3MB)
    테스트 12 〉	통과 (1.10ms, 10.2MB)
    테스트 13 〉	통과 (0.26ms, 10.3MB)
    테스트 14 〉	통과 (0.06ms, 10.3MB)
    테스트 15 〉	통과 (0.05ms, 10.2MB)
    테스트 16 〉	통과 (0.06ms, 10.1MB)
    테스트 17 〉	통과 (0.05ms, 10.3MB)
    테스트 18 〉	통과 (0.85ms, 10.3MB)
    테스트 19 〉	통과 (0.85ms, 10.1MB)
    테스트 20 〉	통과 (1.54ms, 10.3MB)
    테스트 21 〉	통과 (2.87ms, 10.2MB)
    테스트 22 〉	통과 (1.69ms, 10.2MB)
    테스트 23 〉	통과 (0.07ms, 10.4MB)
    테스트 24 〉	통과 (0.91ms, 10.2MB)
    테스트 25 〉	통과 (0.98ms, 10.2MB)
    테스트 26 〉	통과 (1.58ms, 10.3MB)
    테스트 27 〉	통과 (0.17ms, 10.3MB)
    테스트 28 〉	통과 (0.70ms, 9.98MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

---
+++ 후보키.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+def solution(relation):
+    nrow = len(relation)
+    ncol = len(relation[0])
+    candidates = []
+    
+    for i in range(1,ncol+1):
+        for comb in combinations(range(ncol), i):
+            isvalid = True
+            for candidate in candidates:
+                if candidate.issubset(set(comb)):
+                    isvalid = False
+                    break
+            if isvalid:
+                s = {tuple([relation[i][j] for j in comb]) for i in range(nrow)}
+                if len(s) == nrow:
+                    candidates.append(set(comb))
+    return len(candidates)
