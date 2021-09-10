052f8163b7e85c92e11e6b3ba97d11b8c76f53b5
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-09-10 22:44:17 +0900

    Update 더_맵게.py
    정확성  테스트
    테스트 1 〉	통과 (0.00ms, 10.2MB)
    테스트 2 〉	통과 (0.00ms, 10.2MB)
    테스트 3 〉	통과 (0.01ms, 10.1MB)
    테스트 4 〉	통과 (0.01ms, 10.2MB)
    테스트 5 〉	통과 (0.00ms, 10MB)
    테스트 6 〉	실패 (0.40ms, 10.2MB)
    테스트 7 〉	실패 (0.34ms, 10.2MB)
    테스트 8 〉	통과 (0.05ms, 10.2MB)
    테스트 9 〉	통과 (0.04ms, 10.2MB)
    테스트 10 〉	실패 (0.28ms, 10.2MB)
    테스트 11 〉	통과 (0.20ms, 10.1MB)
    테스트 12 〉	통과 (0.66ms, 10MB)
    테스트 13 〉	실패 (0.34ms, 10.1MB)
    테스트 14 〉	통과 (0.01ms, 10.2MB)
    테스트 15 〉	통과 (0.46ms, 10.2MB)
    테스트 16 〉	통과 (0.00ms, 10.1MB)
    효율성  테스트
    테스트 1 〉	통과 (181.57ms, 18.8MB)
    테스트 2 〉	통과 (401.07ms, 27MB)
    테스트 3 〉	통과 (1890.59ms, 63.9MB)
    테스트 4 〉	통과 (153.98ms, 17.3MB)
    테스트 5 〉	통과 (1916.50ms, 71.2MB)
    채점 결과
    정확성: 57.1
    효율성: 23.8
    합계: 81.0 / 100.0

--- 더_맵게.py
+++ 더_맵게.py
@@ -1,25 +1,17 @@
+from heapq import heappush, heappop
+
 def solution(scoville, K):
-    answer = 0
-    li = [x for x in scoville if x < K]
-    hot = len(scoville) - len(li)
-    li.sort(reverse = True)
-    while(li):
-        if len(li) == 1:
-            # if one is not hot enough but at least is 
-            if hot>1:
-                return answer+1
-            else:
-                return -1
-        temp = li.pop() + 2 * li.pop()
-        if temp < K:
-            li.append(temp)
-            for i in  range(len(li)-1,0,-1):
-                if li[i] > li[i-1]:
-                    li[i],li[i-1] = li[i-1],li[i]
-                else:
-                    break
-        else:
-            hot+=1
-        answer +=1
-        
-    return answer
+    cnt = 0
+    h = []
+    for x in scoville:
+        if x < K:
+            heappush(h, x)
+    
+    while h:
+        if len(h) == 1:
+            return -1
+        new = heappop(h) + heappop(h) * 2
+        if new < K:
+            heappush(h, new)
+        cnt += 1
+    return cnt
