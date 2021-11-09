0cbbb35c0ebef3c22c9bc28246d5e5f1062b8142
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-11-09 19:59:34 +0900

    Update 가장_큰_수.py
    정확성  테스트
    테스트 1 〉	실패 (855.02ms, 21.2MB)
    테스트 2 〉	실패 (256.87ms, 16MB)
    테스트 3 〉	실패 (1333.09ms, 24.6MB)
    테스트 4 〉	실패 (2.33ms, 10.5MB)
    테스트 5 〉	실패 (664.41ms, 20.1MB)
    테스트 6 〉	실패 (475.93ms, 18.8MB)
    테스트 7 〉	통과 (0.04ms, 10.4MB)
    테스트 8 〉	통과 (0.03ms, 10.4MB)
    테스트 9 〉	통과 (0.04ms, 10.4MB)
    테스트 10 〉	실패 (0.03ms, 10.4MB)
    테스트 11 〉	통과 (0.02ms, 10.4MB)
    테스트 12 〉	통과 (0.03ms, 10.4MB)
    테스트 13 〉	실패 (0.02ms, 10.4MB)
    테스트 14 〉	실패 (0.03ms, 10.4MB)
    테스트 15 〉	통과 (0.03ms, 10.4MB)
    채점 결과
    정확성: 40.0
    합계: 40.0 / 100.0

--- 가장_큰_수.py
+++ 가장_큰_수.py
@@ -1,29 +1,20 @@
+def expend(num):
+    if num == "0":
+        return 0
+    
+    num = int(num)
+    zero = 1
+    while num % 10 == 0:
+        num //= 10
+        zero += 1
+    
+    num *= 10 ** (4 - len(str(num)))
+    num -= zero
+    
+    return num
+
 def solution(numbers):
-    answer = ''
-    size = len(numbers)
-    for i in range(size):
-        max_index = i
-        for j in range(i+1, size):
-            a = numbers[max_index]
-            b = numbers[j]
-            if a < 10:
-                len_a = 1
-            elif a < 100:
-                len_a = 2
-            elif a < 1000:
-                len_a = 3
-            else:
-                len_a = 4
-            if b < 10:
-                len_b = 1
-            elif a < 100:
-                len_b = 2
-            elif a < 1000:
-                len_b = 3
-            else:
-                len_b = 4
-            if a*(10**len_b)+b < b*(10**len_a)+a:
-                max_index = j
-        numbers[max_index], numbers[i] = numbers[i], numbers[max_index]
-        answer+=str(numbers[i])
-    return str(int(answer))
+    numbers = list(map(str, numbers))
+    numbers.sort(reverse=True, key=expend)
+        
+    return str(int("".join(numbers)))
