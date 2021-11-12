bc4831cd48ae645a0f6fb5c7fce6cdfde88ddb3c
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-11-13 01:07:14 +0900

    Update 모의고사.py

--- 모의고사.py
+++ 모의고사.py
@@ -1,23 +1,37 @@
+class Student:
+    def __init__(self, n, pattern):
+        self.n = n
+        self.pattern = pattern
+        self.size = len(pattern)
+        self.score = 0
+    
+    def grade_score(self, answers):
+        for i,a in enumerate(answers):
+            if a == self.pattern[i%self.size]:
+                self.score += 1
+                
+    @classmethod
+    def top(cls, students):
+        top_score = -1
+        
+        for student in students:
+            if top_score < student.score:
+                top_score = student.score
+                top_students = [student.n]
+            elif top_score == student.score:
+                top_students.append(student.n)
+        
+        return top_students
+                
+
 def solution(answers):
-    answer = []
-    st1 = [1,2,3,4,5]
-    st2 = [2,1,2,3,2,4,2,5]
-    st3 = [3,3,1,1,2,2,4,4,5,5] 
-    score1=0
-    score2=0
-    score3=0
-    for i,a in enumerate(answers):
-        if a == st1[i%5]:
-            score1+=1
-        if a == st2[i%8]:
-            score2+=1
-        if a == st3[i%10]:
-            score3+=1
-    m = max([score1,score2,score3])
-    if m == score1:
-        answer.append(1)
-    if m == score2:
-        answer.append(2)
-    if m == score3:
-        answer.append(3)
-    return answer
+    students = [
+        Student(1, [1,2,3,4,5]),
+        Student(2, [2,1,2,3,2,4,2,5]),
+        Student(3, [3,3,1,1,2,2,4,4,5,5] )       
+               ]
+    
+    for student in students:
+        student.grade_score(answers)
+        
+    return Student.top(students)
