8a4735dba23d9f84fc568288d3fd14b128aa2ef7
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-11-18 20:05:39 +0900

    Update 단속카메라.py
    정확성  테스트
    테스트 1 〉	통과 (0.06ms, 10.2MB)
    테스트 2 〉	통과 (0.04ms, 10.2MB)
    테스트 3 〉	통과 (0.06ms, 10.2MB)
    테스트 4 〉	통과 (0.06ms, 10.2MB)
    테스트 5 〉	통과 (0.06ms, 10.2MB)
    효율성  테스트
    테스트 1 〉	통과 (3.35ms, 10.4MB)
    테스트 2 〉	통과 (1.76ms, 10.3MB)
    테스트 3 〉	통과 (10.72ms, 10.4MB)
    테스트 4 〉	통과 (0.32ms, 10.2MB)
    테스트 5 〉	통과 (13.28ms, 10.6MB)
    채점 결과
    정확성: 50.0
    효율성: 50.0
    합계: 100.0 / 100.0

--- 단속카메라.py
+++ 단속카메라.py
@@ -3,7 +3,7 @@ def solution(routes):
     routes.sort(key=lambda x: x[0], reverse=True)
     while routes:
         camera = min(routes, key=lambda x:x[1])[1]
-        while routes[-1][0] < camera:
+        while routes[-1][0] <= camera:
             routes.pop()
             if not routes:
                 break
