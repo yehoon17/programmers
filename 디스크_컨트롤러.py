409b40c73271a11fac1ecc770bc5a9c02329de0e
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-11-20 21:27:00 +0900

    Create 디스크_컨트롤러.py
    정확성  테스트
    테스트 1 〉	통과 (0.64ms, 10.3MB)
    테스트 2 〉	통과 (0.51ms, 10.3MB)
    테스트 3 〉	통과 (0.46ms, 10.4MB)
    테스트 4 〉	통과 (0.47ms, 10.3MB)
    테스트 5 〉	통과 (0.55ms, 10.2MB)
    테스트 6 〉	통과 (0.05ms, 10.3MB)
    테스트 7 〉	통과 (0.42ms, 10.4MB)
    테스트 8 〉	통과 (0.34ms, 10.3MB)
    테스트 9 〉	통과 (0.12ms, 10.3MB)
    테스트 10 〉	통과 (0.66ms, 10.3MB)
    테스트 11 〉	통과 (0.02ms, 10.3MB)
    테스트 12 〉	통과 (0.02ms, 10.3MB)
    테스트 13 〉	통과 (0.02ms, 10.3MB)
    테스트 14 〉	통과 (0.01ms, 10.3MB)
    테스트 15 〉	통과 (0.01ms, 10.3MB)
    테스트 16 〉	통과 (0.02ms, 10.3MB)
    테스트 17 〉	통과 (0.02ms, 10.3MB)
    테스트 18 〉	통과 (0.01ms, 10.2MB)
    테스트 19 〉	통과 (0.01ms, 10.4MB)
    테스트 20 〉	통과 (0.01ms, 10.3MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

---
+++ 디스크_컨트롤러.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, start, size, nextNode):
+        self.start = start
+        self.size = size
+        self.nextNode = nextNode
+
+    def find(self, t):
+        earliest_node = self
+        previous = self
+        node = self.nextNode
+        while node.start > t:
+            if node.start < earliest_node.nextNode.start:
+                earliest_node = previous
+            previous = node
+            node = node.nextNode
+            if not node:
+                previous = earliest_node
+                node = earliest_node.nextNode
+                break
+        previous.nextNode = node.nextNode
+        
+        return node
+    
+
+def solution(jobs):
+    jobs.sort(key=lambda x: x[1])
+    
+    tail = None
+    nextNode = tail
+    for start, size in jobs[::-1]:
+        node = Node(start, size, nextNode)
+        nextNode = node
+    head = Node(1001, -1, nextNode)
+    
+    t = 0
+    total = 0
+    node = head
+    while head.nextNode:
+        node = head.find(t)
+        s = node.size + max(0, t - node.start)
+        total += s
+        t = max(t, node.start) + node.size
+        
+    return total // len(jobs)
