223159f2ccc35ac10fce493be3e93179581301ab
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-11-24 09:39:58 +0900

    Update 사칙연산.py
    정확성  테스트
    테스트 1 〉	통과 (0.04ms, 10.4MB)
    테스트 2 〉	실패 (0.08ms, 10.4MB)
    테스트 3 〉	통과 (0.02ms, 10.4MB)
    테스트 4 〉	통과 (0.02ms, 10.4MB)
    테스트 5 〉	통과 (1.48ms, 10.4MB)
    테스트 6 〉	통과 (2.15ms, 10.5MB)
    테스트 7 〉	실패 (0.14ms, 10.4MB)
    테스트 8 〉	통과 (0.12ms, 10.5MB)
    테스트 9 〉	통과 (0.03ms, 10.4MB)
    테스트 10 〉	통과 (0.02ms, 10.5MB)
    효율성  테스트
    테스트 1 〉	실패 (시간 초과)
    테스트 2 〉	실패 (시간 초과)
    테스트 3 〉	실패 (시간 초과)
    테스트 4 〉	실패 (시간 초과)
    테스트 5 〉	통과 (0.08ms, 10.3MB)
    테스트 6 〉	통과 (0.08ms, 10.4MB)
    테스트 7 〉	실패 (시간 초과)
    테스트 8 〉	실패 (시간 초과)
    채점 결과
    정확성: 44.4
    효율성: 11.1
    합계: 55.6 / 100.0

--- 사칙연산.py
+++ 사칙연산.py
@@ -1,4 +1,8 @@
 from collections import deque
+from itertools import islice
+
+def dq_slice(arr, start):
+    return deque(islice(arr, start, len(arr)))
 
 def max_result(arr):
     if not arr:
@@ -11,7 +15,12 @@ def max_result(arr):
             break
         elif not x == "+":
             result += int(x)
-    return result - min_result(arr)
+    if arr:
+        result0 = result - int(arr[0]) + max_result(dq_slice(arr, 1))
+        result1 = result - min_result(arr)
+        return max(result0, result1)
+    else:
+        return result
 
 def min_result(arr):
     if not arr:
@@ -24,7 +33,12 @@ def min_result(arr):
             break
         elif not x == "+":
             result += int(x)
-    return result - max_result(arr)
+    if arr:
+        result0 = result - int(arr[0]) + min_result(dq_slice(arr, 1))
+        result1 = result - max_result(arr)
+        return min(result0, result1)
+    else:
+        return result
 
 def solution(arr):
     arr = deque(arr)
