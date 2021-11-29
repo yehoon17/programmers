0c727b215761bfffbc480b411ec693a98ef38d62
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-11-29 18:16:02 +0900

    Update 호텔_방_배정.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.2MB)
    테스트 2 〉	통과 (0.01ms, 10.2MB)
    테스트 3 〉	통과 (0.01ms, 10.3MB)
    테스트 4 〉	통과 (0.07ms, 10.2MB)
    테스트 5 〉	통과 (0.01ms, 10.2MB)
    테스트 6 〉	통과 (0.03ms, 10.3MB)
    테스트 7 〉	통과 (0.03ms, 10.3MB)
    테스트 8 〉	통과 (0.01ms, 10.3MB)
    테스트 9 〉	통과 (0.01ms, 10.2MB)
    테스트 10 〉	통과 (0.01ms, 10.2MB)
    테스트 11 〉	통과 (0.01ms, 10.2MB)
    테스트 12 〉	통과 (0.02ms, 10.2MB)
    테스트 13 〉	통과 (0.01ms, 10.3MB)
    테스트 14 〉	통과 (0.01ms, 10.4MB)
    테스트 15 〉	통과 (0.05ms, 10.2MB)
    테스트 16 〉	통과 (0.08ms, 10.2MB)
    테스트 17 〉	통과 (0.09ms, 10.2MB)
    테스트 18 〉	통과 (0.36ms, 10.3MB)
    테스트 19 〉	통과 (0.38ms, 10.4MB)
    테스트 20 〉	통과 (0.43ms, 10.4MB)
    테스트 21 〉	통과 (0.65ms, 10.4MB)
    테스트 22 〉	통과 (0.82ms, 10.4MB)
    테스트 23 〉	통과 (0.83ms, 10.4MB)
    테스트 24 〉	통과 (0.98ms, 10.5MB)
    테스트 25 〉	통과 (0.02ms, 10.2MB)
    테스트 26 〉	통과 (0.04ms, 10.3MB)
    효율성  테스트
    테스트 1 〉	통과 (467.74ms, 111MB)
    테스트 2 〉	통과 (438.10ms, 68.6MB)
    테스트 3 〉	통과 (591.67ms, 191MB)
    테스트 4 〉	통과 (592.30ms, 191MB)
    테스트 5 〉	실패 (시간 초과)
    테스트 6 〉	실패 (시간 초과)
    테스트 7 〉	실패 (시간 초과)
    채점 결과
    정확성: 78.8
    효율성: 12.1
    합계: 90.9 / 100.0

--- 호텔_방_배정.py
+++ 호텔_방_배정.py
@@ -1,6 +1,6 @@
 def solution(k, room_number):
     answer = []
-    redirect = [i for i in range(k+1)]
+    redirect = {i: i for i in range(k+1)}
     for request in room_number:
         requested = []
         while True:
