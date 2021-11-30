8dc60f68ec3351eab4e4825452da6496710ba5ad
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-11-30 09:01:32 +0900

    Update 징검다리_건너기.py
    정확성  테스트
    테스트 1 〉	통과 (0.00ms, 10.3MB)
    테스트 2 〉	통과 (0.01ms, 10.3MB)
    테스트 3 〉	통과 (0.01ms, 10.2MB)
    테스트 4 〉	통과 (0.04ms, 10.3MB)
    테스트 5 〉	통과 (0.05ms, 10.2MB)
    테스트 6 〉	통과 (0.23ms, 10.3MB)
    테스트 7 〉	통과 (0.45ms, 10.3MB)
    테스트 8 〉	통과 (0.47ms, 10.3MB)
    테스트 9 〉	통과 (0.45ms, 10.3MB)
    테스트 10 〉	통과 (0.04ms, 10.2MB)
    테스트 11 〉	통과 (0.02ms, 10.2MB)
    테스트 12 〉	통과 (0.03ms, 10.2MB)
    테스트 13 〉	통과 (0.05ms, 10.3MB)
    테스트 14 〉	통과 (0.23ms, 10.2MB)
    테스트 15 〉	통과 (0.44ms, 10.2MB)
    테스트 16 〉	통과 (0.45ms, 10.2MB)
    테스트 17 〉	통과 (0.43ms, 10.2MB)
    테스트 18 〉	통과 (0.01ms, 10.2MB)
    테스트 19 〉	통과 (0.03ms, 10.2MB)
    테스트 20 〉	통과 (0.05ms, 10.2MB)
    테스트 21 〉	통과 (0.22ms, 10.2MB)
    테스트 22 〉	통과 (0.47ms, 10.2MB)
    테스트 23 〉	통과 (0.47ms, 10.2MB)
    테스트 24 〉	통과 (0.47ms, 10.2MB)
    테스트 25 〉	통과 (0.02ms, 10.2MB)
    효율성  테스트
    테스트 1 〉	통과 (94.46ms, 18.4MB)
    테스트 2 〉	통과 (85.22ms, 18.6MB)
    테스트 3 〉	통과 (94.93ms, 18.5MB)
    테스트 4 〉	통과 (95.07ms, 18.6MB)
    테스트 5 〉	통과 (94.90ms, 18.7MB)
    테스트 6 〉	통과 (96.73ms, 18.5MB)
    테스트 7 〉	통과 (87.65ms, 18.5MB)
    테스트 8 〉	통과 (71.36ms, 18.5MB)
    테스트 9 〉	통과 (79.67ms, 18.6MB)
    테스트 10 〉	통과 (78.75ms, 18.5MB)
    테스트 11 〉	통과 (87.80ms, 18.5MB)
    테스트 12 〉	통과 (79.59ms, 18.6MB)
    테스트 13 〉	통과 (65.33ms, 19.4MB)
    테스트 14 〉	통과 (74.57ms, 18.5MB)
    채점 결과
    정확성: 64.1
    효율성: 35.9
    합계: 100.0 / 100.0

--- 징검다리_건너기.py
+++ 징검다리_건너기.py
@@ -1,15 +1,15 @@
 from collections import deque
 
 def solution(stones, k):
-    tmp = deque()
+    answer = 200000001
+    k_window = deque()
     
-    for _ in range(k):
-        tmp.append(stones.pop())
-        
-    answer = max(tmp)
-    while stones:
-        tmp.append(stones.pop())
-        tmp.popleft()
-        answer = min(max(tmp), answer)
-        
+    for i, stone in enumerate(stones):
+        while k_window and stones[k_window[-1]] <= stone:
+            k_window.pop()
+        k_window.append(i)
+        if k_window[0] == i - k:
+            k_window.popleft()
+        if i >= k - 1:
+            answer = min(answer, stones[k_window[0]])
     return answer
