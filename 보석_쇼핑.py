12ca2e9969a67754f934e51a8538d658816bac5f
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-11-30 14:24:16 +0900

    Update 보석_쇼핑.py
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.3MB)
    테스트 2 〉	통과 (0.04ms, 10.2MB)
    테스트 3 〉	통과 (0.13ms, 10.2MB)
    테스트 4 〉	통과 (0.13ms, 10.3MB)
    테스트 5 〉	통과 (0.32ms, 10.2MB)
    테스트 6 〉	통과 (0.01ms, 10.3MB)
    테스트 7 〉	통과 (0.01ms, 10.2MB)
    테스트 8 〉	통과 (0.26ms, 10.3MB)
    테스트 9 〉	통과 (0.36ms, 10.3MB)
    테스트 10 〉	통과 (0.29ms, 10.3MB)
    테스트 11 〉	통과 (0.51ms, 10.3MB)
    테스트 12 〉	통과 (0.55ms, 10.2MB)
    테스트 13 〉	통과 (0.70ms, 10.4MB)
    테스트 14 〉	통과 (0.87ms, 10.4MB)
    테스트 15 〉	통과 (1.40ms, 10.5MB)
    효율성  테스트
    테스트 1 〉	통과 (2.17ms, 10.5MB)
    테스트 2 〉	통과 (2.83ms, 10.6MB)
    테스트 3 〉	통과 (5.78ms, 11.2MB)
    테스트 4 〉	통과 (7.30ms, 11.9MB)
    테스트 5 〉	통과 (10.52ms, 11.9MB)
    테스트 6 〉	통과 (11.61ms, 12.1MB)
    테스트 7 〉	통과 (14.21ms, 12.7MB)
    테스트 8 〉	통과 (16.56ms, 12.9MB)
    테스트 9 〉	통과 (17.95ms, 13.4MB)
    테스트 10 〉	통과 (18.61ms, 13.9MB)
    테스트 11 〉	통과 (21.00ms, 14.7MB)
    테스트 12 〉	통과 (22.30ms, 15.4MB)
    테스트 13 〉	통과 (33.17ms, 16.4MB)
    테스트 14 〉	통과 (37.02ms, 17MB)
    테스트 15 〉	통과 (38.10ms, 17.8MB)
    채점 결과
    정확성: 33.3
    효율성: 66.7
    합계: 100.0 / 100.0

--- 보석_쇼핑.py
+++ 보석_쇼핑.py
@@ -3,12 +3,13 @@ def solution(gems):
     answer = [0, size]
     index_of = {}
     total = len(set(gems))
-    # start = size
-    # end = -1
+    min_gem = gems[0]
     for i, gem in enumerate(gems):
         index_of[gem] = i
+        if min_gem == gem:
+            min_gem = min(index_of, key=lambda x: index_of[x])
         if len(index_of) == total:
-            start = min(index_of.values())
+            start = index_of[min_gem]
             end = i
             if answer[1] - answer[0] > end - start:
                 answer = [start+1, end+1]
