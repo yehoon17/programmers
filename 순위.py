33f3adb230d5eecfdc2e01785ed297157e137708
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-12-06 21:34:42 +0900

    Update 순위.py
    정확성  테스트
    테스트 1 〉	통과 (0.03ms, 10.3MB)
    테스트 2 〉	통과 (0.03ms, 10.3MB)
    테스트 3 〉	통과 (0.14ms, 10.4MB)
    테스트 4 〉	통과 (0.06ms, 10.3MB)
    테스트 5 〉	통과 (6.11ms, 10.5MB)
    테스트 6 〉	통과 (11.82ms, 10.4MB)
    테스트 7 〉	통과 (112.04ms, 10.5MB)
    테스트 8 〉	통과 (406.43ms, 10.8MB)
    테스트 9 〉	통과 (721.20ms, 10.9MB)
    테스트 10 〉	통과 (853.76ms, 11.2MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- 순위.py
+++ 순위.py
@@ -1,28 +1,40 @@
+def winners_of(winner, win_from, n):
+    visited = [False for _ in range(n+1)]
+    winners = []
+    bfs = {winner}
+    while bfs:
+        w = bfs.pop()
+        if not visited[w]:
+            visited[w] = True
+            winners.append(w)
+            bfs = bfs.union(win_from[w])
+
+    return winners
+
+def losers_of(loser, lose_to, n):
+    visited = [False for _ in range(n+1)]
+    losers = []
+    bfs = {loser}
+    while bfs:
+        l = bfs.pop()
+        if not visited[l]:
+            visited[l] = True
+            losers.append(l)
+            bfs = bfs.union(lose_to[l])
+
+    return losers
+    
 def solution(n, results):
     win_from = {i+1: set() for i in range(n)}
     lose_to = {i+1: set() for i in range(n)}
     for winner, loser in results:
-        win_from[loser].add(winner)
-        visit_win = [False for _ in range(n+1)]
-        bfs = {winner}
-        while bfs:
-            w = bfs.pop()
-            if not visit_win[w]:
-                visit_win[w] = True
-                winners = win_from[w]
-                win_from[loser] = win_from[loser].union(winners)
-                bfs = bfs.union(winners)
-            
-        lose_to[winner].add(loser)
-        visit_lose = [False for _ in range(n+1)]
-        bfs = {loser}
-        while bfs:
-            l = bfs.pop()
-            if not visit_lose[l]:
-                visit_lose[l] = True
-                losers = lose_to[l]
-                lose_to[winner] = lose_to[winner].union(losers)
-                bfs = bfs.union(losers)
+        winners = winners_of(winner, win_from, n)
+        losers = losers_of(loser, lose_to, n)
+        for w in winners:
+            for l in losers:
+                win_from[l].add(w)
+                lose_to[w].add(l)
+        
     cnt = 0
     for i in range(1, 1+n):
         if len(win_from[i]) + len(lose_to[i]) == n-1:
