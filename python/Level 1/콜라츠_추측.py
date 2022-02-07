247c34de79cadfa158d5a13972c5801a47f0e524
yehoon17 <yehoon17@gmail.com>
2022-02-08 06:30:55 +0900

    Update 콜라츠_추측.py

--- python/Level 1/콜라츠_추측.py
+++ python/Level 1/콜라츠_추측.py
@@ -1,12 +1,10 @@
 def solution(num):
-    answer = 0
-    while(answer < 500):
+    for answer in range(500):
         if num == 1:
             return answer
         if num % 2 == 0:
             num //= 2
         else:
-            num = 3*num+1
-        answer += 1
-        
+            num = 3*num + 1
+
     return -1
