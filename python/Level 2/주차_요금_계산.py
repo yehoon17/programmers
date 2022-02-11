8837287baec2e1feb775b508381185660ddb85df
yehoon17 <yehoon17@gmail.com>
2022-02-11 20:47:23 +0900

    Update 주차_요금_계산.py
    정확성  테스트
    테스트 1 〉	통과 (0.04ms, 10.4MB)
    테스트 2 〉	통과 (0.03ms, 10.4MB)
    테스트 3 〉	통과 (0.06ms, 10.3MB)
    테스트 4 〉	통과 (0.14ms, 10.4MB)
    테스트 5 〉	통과 (0.18ms, 10.5MB)
    테스트 6 〉	통과 (0.21ms, 10.4MB)
    테스트 7 〉	통과 (1.52ms, 10.4MB)
    테스트 8 〉	통과 (0.82ms, 10.5MB)
    테스트 9 〉	통과 (0.20ms, 10.6MB)
    테스트 10 〉	통과 (2.24ms, 10.4MB)
    테스트 11 〉	통과 (3.55ms, 10.7MB)
    테스트 12 〉	통과 (1.92ms, 10.7MB)
    테스트 13 〉	통과 (0.04ms, 10.5MB)
    테스트 14 〉	통과 (0.03ms, 10.4MB)
    테스트 15 〉	통과 (0.03ms, 10.4MB)
    테스트 16 〉	통과 (0.02ms, 10.3MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- python/Level 2/주차_요금_계산.py
+++ python/Level 2/주차_요금_계산.py
@@ -2,21 +2,26 @@ def str_to_min(time):
     hour, minute = time.split(":")
     return int(hour)*60 + int(minute)
 
+
 def n_unit(extra_time, unit_time):
     if extra_time % unit_time == 0:
         return extra_time // unit_time
-    return max(int(extra_time/unit_time) + 1, 0)
+    return int(extra_time/unit_time) + 1
 
 
 def get_fee(total_time, fees):
     base_time, base_fee, unit_time, unit_fee = fees
     extra_time = total_time - base_time
+    if extra_time <= 0:
+        return base_fee
+
     extra_fee = n_unit(extra_time, unit_time)*unit_fee
     return base_fee + extra_fee
 
+
 def solution(fees, records):
     history = {}
-    
+
     for record in records:
         time, car, stat = record.split()
         if car not in history:
@@ -25,15 +30,17 @@ def solution(fees, records):
             if stat == "IN":
                 history[car][1] = time
             else:
-                history[car][2] += str_to_min(time) - str_to_min(history[car][1])
+                history[car][2] += str_to_min(time) - \
+                    str_to_min(history[car][1])
             history[car][0] = stat
-            
+
     for car in history:
         if history[car][0] == "IN":
-            history[car][2] += str_to_min("23:59") - str_to_min(history[car][1])
-            
+            history[car][2] += str_to_min("23:59") - \
+                str_to_min(history[car][1])
+
     cars = [car for car in history]
     cars.sort()
     payments = [get_fee(history[car][2], fees) for car in cars]
-    
+
     return payments
